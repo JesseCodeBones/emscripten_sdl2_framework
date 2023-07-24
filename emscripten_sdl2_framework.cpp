40ad8a01ed8393c1d1c0a23e7df892783a88ed8c
Jesse Chen <ruixiang.chen@partner.bmw.com>
2023-07-24 14:16:22 +0800

    gtest

---
+++ emscripten_sdl2_framework.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void say_hello(){
+    std::cout << "Hello, from emscripten_sdl2_framework!\n";
+}
---
+++ test/maintest.cpp
@@ -0,0 +1,4 @@
+#include <gtest/gtest.h>
+TEST(emscripten_sdl2_framework_test, e2e) {
+  ASSERT_EQ(42, 42);
+}
